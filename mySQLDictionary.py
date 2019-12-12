c4996ff6c0436f4e99068d9273e27d7487ca6a6f
Kevin <mrhallonline@gmail.com>
2019-12-12 15:03:40 -0500

    mysql dictionary completed. Can be improved with difflib

--- mySQLDictionary.py
+++ mySQLDictionary.py
@@ -6,3 +6,15 @@ con = mysql.connector.connect(
     host = "108.167.140.122",
     database = "ardit700_pm1database"
 )
+
+cursor = con.cursor()
+user_word = input("Enter the word to define: ")
+query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" %user_word)
+results = cursor.fetchall()
+
+
+if results:
+    for result in results:
+        print(result[1])
+else:
+    print("This word was not found")
