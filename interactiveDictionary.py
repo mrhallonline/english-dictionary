391eb347bf0e4f6d84dbc14e4439dea034b0c81c
Kevin <mrhallonline@gmail.com>
2019-12-12 02:19:40 -0500

    need to fix definition output

--- interactiveDictionary.py
+++ interactiveDictionary.py
@@ -11,27 +11,33 @@ with open('data.json') as di:
         corpus = json.load(di)
 
 
+#convert list to string and newlines
+def list_to_string(old_list):
+    new_string = old_list
+    return new_string
 
 #function to look up user inputted word
 #check if word is valid to avoid traceback error before reading from database
 def lookup_word(word):
     if user_word in corpus: 
-        return corpus[word]
+        definition = corpus[word]
+        return list_to_string(definition)
+         
     elif len(get_close_matches(user_word, corpus, n=3, cutoff=0.8)) > 0:
         close_word = get_close_matches(user_word, corpus, n=3, cutoff=0.8)[0]
-        return "Did you mean to type " + close_word + '?'
+        user_answer = (input("Did you mean to type %s? Enter Y for yes, or N for no: "%close_word)).lower()
+        if user_answer == 'y':
+            return corpus[close_word]
+        elif user_answer == 'n':
+            return "Sorry %s doesn't match any other words in the dictionary. Check spelling and try again" %word
+        else:
+            return "Sorry that wasn't one of the options."
     else:
         return "Doesn't match any words in dictionary. Check spelling"
     
     
 #ask for user input and makes all lowercase
 user_word = (input("Enter word to define: ")).lower()
-
-#difflib sequence SequenceMatcher
-# SequenceMatcher(None, "rainn", "rain").ratio()
-
-
-
 
 
 print(lookup_word(user_word))
